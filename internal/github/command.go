71868db5d5b9f0aaf22b734d5bf7622584fabc35
Nicolas Zin <nicolas.zin@gmail.com>
2023-07-09 01:30:49 -0400

    removing commands

--- internal/github/command.go
+++
@@ -1,9 +0,0 @@
-package github
-
-/**
- * Each command/mutation we want to perform will be isloated into a GithubCommand
- * object, so we can regroup all of them to apply (or cancel) them in batch
- */
-type GithubCommand interface {
-	Apply() error
-}
